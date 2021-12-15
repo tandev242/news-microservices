58e02eb776167495e4affeb446622fefc0e6a75d
tran quoc huy <tranquochuy221096@gmail.com>
2021-12-15 10:58:20 +0700

    test kafka 1

--- crawler/index.js
+++ crawler/index.js
@@ -1,103 +1,113 @@
-const express = require("express");
-require("dotenv").config();
-const Request = require("request");
-const mongoose = require("mongoose");
-const cheerio = require("cheerio");
-const fs = require("fs").promises;
-var cron = require("node-cron");
-const Posts = require("./post.model");
-const Categories = require("./category.model");
+const express = require('express')
+require('dotenv').config()
+const Request = require('request')
+const mongoose = require('mongoose')
+const cheerio = require('cheerio')
+const fs = require('fs').promises
+var cron = require('node-cron')
+const Posts = require('./post.model')
+const Categories = require('./category.model')
 
-const app = express();
+const kafka = require('kafka-node')
+const Producer = kafka.Producer
+const client = new kafka.KafkaClient({ kafkaHost: '127.0.0.1:9092' })
+const producer = new Producer(client)
+const topic = 'post'
+
+producer.on('ready', () => {
+  console.log('connected')
+})
+
+const app = express()
 
 try {
-    mongoose.connect("mongodb://localhost:27017/crawl", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to database");
+  mongoose.connect('mongodb://localhost:27017/crawl', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log('Connected to database')
 } catch (error) {
-    console.log(error);
+  console.log(error)
 }
 
 // Get list post
 async function getListPost(link) {
-    return new Promise((resolve, reject) => {
-        Request(
-            {
-                method: "GET",
-                url: link,
-            },
-            function (err, response, body) {
-                if (err) reject(err);
-                resolve(JSON.parse(body));
-            }
-        );
-    });
+  return new Promise((resolve, reject) => {
+    Request(
+      {
+        method: 'GET',
+        url: link,
+      },
+      function (err, response, body) {
+        if (err) reject(err)
+        resolve(JSON.parse(body))
+      }
+    )
+  })
 }
 
 // Get post content
 async function getContent(link) {
-    return new Promise(async (resolve, reject) => {
-        Request(
-            {
-                method: "GET",
-                url: link,
-            },
-            function (err, response, body) {
-                if (err) reject(err);
-                if (!body || typeof body !== "string") {
-                    reject(new Error("Can' load url"));
-                } else {
-                    const $ = cheerio.load(body);
-                    var content = $(
-                        "body > section.section.page-detail.top-detail > div > div.sidebar-1 > article"
-                    ).html();
-                    resolve(content);
-                }
-            }
-        );
-    });
+  return new Promise(async (resolve, reject) => {
+    Request(
+      {
+        method: 'GET',
+        url: link,
+      },
+      function (err, response, body) {
+        if (err) reject(err)
+        if (!body || typeof body !== 'string') {
+          reject(new Error("Can' load url"))
+        } else {
+          const $ = cheerio.load(body)
+          var content = $(
+            'body > section.section.page-detail.top-detail > div > div.sidebar-1 > article'
+          ).html()
+          resolve(content)
+        }
+      }
+    )
+  })
 }
 
 let listParentCategory = [
-    {
-        categoryId: "1001005",
-        name: "Thời sự",
-    },
-    {
-        categoryId: "1003450",
-        name: "Góc nhìn",
-    },
-    {
-        categoryId: "1001002",
-        name: "Thế giới",
-    },
-    {
-        categoryId: "1003159",
-        name: "Kinh doanh",
-    },
-    {
-        categoryId: "1001009",
-        name: "Khoa học",
-    },
-    {
-        categoryId: "1002691",
-        name: "Giải trí",
-    },
-    {
-        categoryId: "1001007",
-        name: "Pháp luật",
-    },
-    {
-        categoryId: "1003497",
-        name: "Giáo dục",
-    },
-    {
-        categoryId: "1001012",
-        name: "Ý kiến",
-    },
-];
+  {
+    categoryId: '1001005',
+    name: 'Thời sự',
+  },
+  {
+    categoryId: '1003450',
+    name: 'Góc nhìn',
+  },
+  {
+    categoryId: '1001002',
+    name: 'Thế giới',
+  },
+  {
+    categoryId: '1003159',
+    name: 'Kinh doanh',
+  },
+  {
+    categoryId: '1001009',
+    name: 'Khoa học',
+  },
+  {
+    categoryId: '1002691',
+    name: 'Giải trí',
+  },
+  {
+    categoryId: '1001007',
+    name: 'Pháp luật',
+  },
+  {
+    categoryId: '1003497',
+    name: 'Giáo dục',
+  },
+  {
+    categoryId: '1001012',
+    name: 'Ý kiến',
+  },
+]
 
 // Save list parent category
 // (async () => {
@@ -112,127 +122,137 @@ let listParentCategory = [
 
 // crawl function
 const crawl = async (category_id) => {
-    // let category_id = "1001002";
-    let limit = "1000";
-    let link = `https://gw.vnexpress.net/mv?site_id=1000000&category_id=${category_id}&type=1&limit=${limit}&data_select=article_id,article_type,title,share_url,thumbnail_url,publish_time,lead,privacy,original_cate,article_category`;
-    try {
-        const listPost = await getListPost(link);
+  // let category_id = "1001002";
+  let limit = '1000'
+  let link = `https://gw.vnexpress.net/mv?site_id=1000000&category_id=${category_id}&type=1&limit=${limit}&data_select=article_id,article_type,title,share_url,thumbnail_url,publish_time,lead,privacy,original_cate,article_category`
+  try {
+    const listPost = await getListPost(link)
 
-        if (
-            listPost.data[category_id].data &&
-            listPost.data[category_id].data !== []
-        ) {
-            Promise.all(
-                listPost.data[category_id].data.map((e) => {
-                    return new Promise(async (resolve, reject) => {
-                        let content = await getContent(e.share_url);
+    if (
+      listPost.data[category_id].data &&
+      listPost.data[category_id].data !== []
+    ) {
+      Promise.all(
+        listPost.data[category_id].data.map((e) => {
+          return new Promise(async (resolve, reject) => {
+            let content = await getContent(e.share_url)
 
-                        e.content = content;
+            e.content = content
 
-                        resolve(e);
-                    });
-                })
-            ).then(async (values) => {
-                try {
-                    const listPost = values.map((e) => {
-                        let newPost = {
-                            publish_time: e.publish_time,
-                            title: e.title,
-                            postId: e.article_id,
-                            categoryId: e.original_cate,
-                            lead: e.lead,
-                            thumbnail_url: e.thumbnail_url,
-                            content: e.content,
-                        };
+            resolve(e)
+          })
+        })
+      ).then(async (values) => {
+        try {
+          const postList = values.map((e) => {
+            let newPost = {
+              publish_time: e.publish_time,
+              title: e.title,
+              postId: e.article_id,
+              categoryId: e.original_cate,
+              lead: e.lead,
+              thumbnail_url: e.thumbnail_url,
+              content: e.content,
+            }
+            return newPost
+          })
+          producer.send(
+            [{ topic: 'post', messages: postList[0] }],
+            function (err, data) {
+              console.log(data)
+            }
+          )
+          //   const listCate = values.map((e) => {
+          //     let newCate = {
+          //       categoryId: e.original_cate,
+          //       name: e.article_category.cate_name,
+          //       parent_id: e.article_category.full_parent,
+          //     }
+          //     return newCate
+          //   })
 
-                        return newPost;
-                    });
-                    await Posts.create(listPost);
-
-                    const listCate = values.map((e) => {
-                        let newCate = {
-                            categoryId: e.original_cate,
-                            name: e.article_category.cate_name,
-                            parent_id: e.article_category.full_parent,
-                        };
-                        return newCate;
-                    });
-
-                    await Categories.create(listCate);
-                } catch (error) {
-                    if (error.code !== 11000) {
-                        console.log(error);
-                    }
-                }
-            });
+          //   await Categories.create(listCate)
+        } catch (error) {
+          if (error.code !== 11000) {
+            console.log(error)
+          }
         }
-        // for (let e of listPost.data[category_id].data) {
-        //     let content = await getContent(e.share_url);
-
-        //     e.content = content;
-
-        //     let newObj = {
-        //         publish_time: e.publish_time,
-        //         title: e.title,
-        //         postId: e.article_id,
-        //         categoryId: e.original_cate,
-        //         lead: e.lead,
-        //         thumbnail_url: e.thumbnail_url,
-        //         content: e.content,
-        //     };
-
-        //     try {
-        //         await new Posts(newObj).save();
-        //     } catch (error) {
-        //         if (error.code !== 11000) {
-        //             console.log(error);
-        //         }
-        //     }
-
-        //     let newCate = {
-        //         categoryId: e.original_cate,
-        //         name: e.article_category.cate_name,
-        //         parent_id: e.article_category.full_parent,
-        //     };
-
-        //     try {
-        //         await new Categories(newCate).save();
-        //     } catch (error) {
-        //         if (error.code !== 11000) {
-        //             console.log(error);
-        //         }
-        //     }
-        // }
-
-        // await fs.writeFile(
-        //     "data.json",
-        //     JSON.stringify(listPost.data[category_id].data)
-        // );
-    } catch (error) {
-        console.log(error);
+      })
     }
-};
+    // for (let e of listPost.data[category_id].data) {
+    //     let content = await getContent(e.share_url);
+
+    //     e.content = content;
+
+    //     let newObj = {
+    //         publish_time: e.publish_time,
+    //         title: e.title,
+    //         postId: e.article_id,
+    //         categoryId: e.original_cate,
+    //         lead: e.lead,
+    //         thumbnail_url: e.thumbnail_url,
+    //         content: e.content,
+    //     };
+
+    //     try {
+    //         await new Posts(newObj).save();
+    //     } catch (error) {
+    //         if (error.code !== 11000) {
+    //             console.log(error);
+    //         }
+    //     }
+
+    //     let newCate = {
+    //         categoryId: e.original_cate,
+    //         name: e.article_category.cate_name,
+    //         parent_id: e.article_category.full_parent,
+    //     };
+
+    //     try {
+    //         await new Categories(newCate).save();
+    //     } catch (error) {
+    //         if (error.code !== 11000) {
+    //             console.log(error);
+    //         }
+    //     }
+    // }
+
+    // await fs.writeFile(
+    //     "data.json",
+    //     JSON.stringify(listPost.data[category_id].data)
+    // );
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+;(async () => {
+  console.log('Update post & category at 22:40 at Asia/Ho_Chi_Minh timezone')
+  Promise.all(
+    listParentCategory.map(async (e) => {
+      await crawl(e.categoryId)
+    })
+  )
+})()
 
 // crawl all
-cron.schedule(
-    "5 23 * * *",
-    async () => {
-        console.log(
-            "Update post & category at 22:40 at Asia/Ho_Chi_Minh timezone"
-        );
-        Promise.all(
-            listParentCategory.map(async (e) => {
-                await crawl(e.categoryId);
-            })
-        );
-    },
-    {
-        scheduled: true,
-        timezone: "Asia/Ho_Chi_Minh",
-    }
-);
+// cron.schedule(
+//   '26 10 * * *',
+//   async () => {
+//     console.log('Update post & category at 22:40 at Asia/Ho_Chi_Minh timezone')
+//     Promise.all(
+//       listParentCategory.map(async (e) => {
+//         await crawl(e.categoryId)
+//       })
+//     )
+//   },
+//   {
+//     scheduled: true,
+//     timezone: 'Asia/Ho_Chi_Minh',
+//   }
+// )
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log("Server is listening at " + PORT);
-});
+  console.log('Server is listening at ' + PORT)
+})
--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,35 @@
 const express = require('express')
+const kafka = require('kafka-node')
+const fs = require('fs').promises
+const Consumer = kafka.Consumer
+const client = new kafka.KafkaClient({ kafkaHost: '127.0.0.1:9092' })
+const option = {
+  groupId: 'kafka-node-group', //consumer group id, default `kafka-node-group`
+  // Auto commit config
+  autoCommit: true,
+  autoCommitIntervalMs: 5000,
+  // The max wait time is the maximum amount of time in milliseconds to block waiting if insufficient data is available at the time the request is issued, default 100ms
+  fetchMaxWaitMs: 100,
+  // This is the minimum number of bytes of messages that must be available to give a response, default 1 byte
+  fetchMinBytes: 1,
+  // The maximum bytes to include in the message set for this partition. This helps bound the size of the response.
+  fetchMaxBytes: 1024 * 1024,
+  // If set true, consumer will fetch message from the given offset in the payloads
+  fromOffset: false,
+  // If set to 'buffer', values will be returned as raw buffer objects.
+  encoding: 'utf8',
+  keyEncoding: 'utf8',
+}
+const consumer = new Consumer(client, [{ topic: 'post' }], option)
+
+consumer.on('message', async (message) => {
+  await fs.writeFile('./test.json', message.value)
+})
 
 const app = express()
 
 app.use(express.json())
 
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is starting');
+app.listen(process.env.PORT || 3001, () => {
+  console.log('Server is starting')
 })
