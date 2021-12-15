547316fa184b44f302f9c76e8b7e1655a68f24a0
NhatTanUT <tvnt221@gmail.com>
2021-12-15 09:08:34 +0700

    2

---
+++ posts/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is starting');
+})
